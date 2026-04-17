d63272c4225be45732fc49706845821d6114e0ce
Balaji-coder-06 <balajisundar8242@gmail.com>
2026-04-17 13:58:49 +0530

    chor : Added CRUD

--- router/adminRoute/adminAcess.js
+++ router/adminRoute/adminAcess.js
@@ -6,6 +6,8 @@ import db from '../../db/database.js'
 const router = express.Router()
 
 
+// product add 
+
 router.post('/add-product', adminAuth, async (request, response) => {
     const { product_name, price, image_url } = request.body
 
@@ -28,5 +30,59 @@ router.post('/add-product', adminAuth, async (request, response) => {
     })
 
 })
+
+
+// update Product details 
+
+router.put('/upt-product/:id', adminAuth, async (request, response) => {
+    const { product_name, price, image_url } = request.body
+    const { id } = request.params
+
+    if (!product_name || !price || !image_url) {
+        return response.status(400).json({
+            message: 'Above field are Required'
+        })
+    }
+
+    await db.query(`
+        UPDATE products 
+        SET product_name = ? , price = ? , image_url = ?
+        WHERE id = ?
+        `, [product_name, price, image_url, id])
+
+
+
+    response.status(200).json({
+        message: 'Product Updated Succesfully'
+    })
+
+})
+
+
+router.delete('/del-product/:id', adminAuth, async (request, response) => {
+    const { id } = request.params
+
+    const [find_product] = await db.query(`
+            SELECT * FROM products WHERE id = ?
+         `, [id])
+
+    if (find_product.length === 0) {
+        return response.status(404).json({
+            message: 'Product Not found '
+        })
+    }
+
+    await db.query(`
+        DELETE FROM products 
+        WHERE id = ?
+        `, [id])
+
+    response.status(200).json({
+        message: '"Product Deleted Successfully"'
+    })
+
+})
+
+
 
 export default router
