577df1e86d3f66ce6d9c8061521cc41b8228ae71
balaji-devhub <balajisundar8242@gmail.com>
2026-04-30 02:43:48 +0530

    New Db hosted

--- index.js
+++ index.js
@@ -1,43 +1,42 @@
-import express, { response } from 'express'
-import database from './schema/ApplicationSchema.js'
-import dotenv from 'dotenv'
-import userAuth from './router/userRouter/userLogin.js'
-import useAccess from './router/userRouter/userAccess.js'
+import express, { response } from 'express';
+import database from './schema/ApplicationSchema.js';
+import dotenv from 'dotenv';
+import userAuth from './router/userRouter/userLogin.js';
+import useAccess from './router/userRouter/userAccess.js';
 
-import adminAuth from './router/adminRoute/adminAuth.js'
-import adminAccess from './router/adminRoute/adminAcess.js'
+import adminAuth from './router/adminRoute/adminAuth.js';
+import adminAccess from './router/adminRoute/adminAcess.js';
 
+import cors from 'cors';
 
-import cors from "cors";
+dotenv.config();
 
-dotenv.config()
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 3306
+const app = express();
 
-const app = express()
+app.use(express.json());
 
-app.use(express.json())
+database();
 
-database()
+app.use(
+  cors({
+    origin: '*',
+  })
+);
 
-
-app.use(cors({
-    origin: "*",
-}));
-
-// router middleware 
+// router middleware
 
 // user route
-app.use('/user', userAuth, useAccess)
+app.use('/user', userAuth, useAccess);
 
 // admin route
-app.use('/admin', adminAuth, adminAccess)
+app.use('/admin', adminAuth, adminAccess);
 
 app.get('/', (reqest, response) => {
-    response.send('Xon  Server')
-})
-
+  response.send('Xon  Server');
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
